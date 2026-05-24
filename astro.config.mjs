9939c2e18bdb90d6a94b92d769ff8fbf6791a171
Alec Fessler <afessler1998@gmail.com>
2026-05-24 12:36:29 -0500

    Initial commit: Astro blog scaffolded with dark minimalist theme

---
+++ astro.config.mjs
@@ -0,0 +1,53 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig, fontProviders } from 'astro/config';
+
+export default defineConfig({
+	site: 'https://alecfessler.com',
+	integrations: [mdx(), sitemap()],
+	markdown: {
+		shikiConfig: {
+			theme: 'github-dark',
+			langs: [
+				'zig',
+				'hlsl',
+				'glsl',
+				'wgsl',
+				'cpp',
+				'rust',
+				'python',
+				'bash',
+				'json',
+				'typescript',
+				'javascript',
+			],
+			wrap: false,
+		},
+	},
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: 'Atkinson',
+			cssVariable: '--font-atkinson',
+			fallbacks: ['sans-serif'],
+			options: {
+				variants: [
+					{
+						src: ['./src/assets/fonts/atkinson-regular.woff'],
+						weight: 400,
+						style: 'normal',
+						display: 'swap',
+					},
+					{
+						src: ['./src/assets/fonts/atkinson-bold.woff'],
+						weight: 700,
+						style: 'normal',
+						display: 'swap',
+					},
+				],
+			},
+		},
+	],
+});
---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Alec Fessler';
+export const SITE_DESCRIPTION = 'Notes on graphics, audio, and engine work.';
+export const AUTHOR = 'Alec Fessler';
+export const GITHUB_URL = 'https://github.com/AlecFessler';
